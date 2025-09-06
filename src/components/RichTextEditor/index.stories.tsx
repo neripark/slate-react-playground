4f690e8b046b9289015907bb8a229dc4c051961b
neripark <neripark@gmail.com>
2025-09-06 14:22:13 +0900

    center

--- src/components/RichTextEditor/index.stories.tsx
+++ src/components/RichTextEditor/index.stories.tsx
@@ -4,9 +4,6 @@ import { RichTextEditor } from "./index";
 const meta = {
   title: "Components/RichTextEditor",
   component: RichTextEditor,
-  parameters: {
-    layout: "centered",
-  },
   argTypes: {
     initialValue: {
       control: "text",
